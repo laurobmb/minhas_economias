356ec698bb7a99a6eb7e7ea55a2099a284901605
Lauro Gomes <lagomes@redhat.com>
2025-07-09 00:39:48 -0300

    update Wed Jul  9 12:39:48 AM -03 2025 - Ajustando testes

--- auth/user.go
+++ auth/user.go
@@ -69,3 +69,11 @@ func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
